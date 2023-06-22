342615aa6e1ae776acce12406f8bc3f6b19b81ca
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-06-22 13:53:06 -0500

    Initial Commit

---
+++ DropShadows/DropShadowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropShadowsApp.swift
+//  DropShadows
+//
+//  Created by Justin Hold on 6/22/23.
+//
+
+import SwiftUI
+
+@main
+struct DropShadowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
